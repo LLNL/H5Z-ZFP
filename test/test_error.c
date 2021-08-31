debb6bb7e579df66a316c59d8456a9b50dcae523
miller86 <miller86@llnl.gov>
2021-08-31 10:34:15 -0700

    put _GNU_SOURCE b4 all headers

--- test/test_error.c
+++ test/test_error.c
@@ -8,19 +8,16 @@ This file is part of H5Z-ZFP. Please also read the BSD license
 https://raw.githubusercontent.com/LLNL/H5Z-ZFP/master/LICENSE 
 */
 
+#define _GNU_SOURCE
 #include <assert.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#define _GNU_SOURCE
 #include <string.h>
 #include <strings.h>
 #include <unistd.h>
-
-#define _GNU_SOURCE
-#include <string.h>
 
 #include "hdf5.h"
 
