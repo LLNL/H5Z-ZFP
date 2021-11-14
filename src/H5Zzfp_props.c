d1cc74a3f930b14d906c5b3e3e5f3936a4c336c1
Mark (he/his) C. Miller <miller86@llnl.gov>
2021-11-14 15:57:47 -0800

    Create H5Zzfp_props.c
    Lets fix this slightly differently. Lets remove any use of `uint` anywhere here. The header file for the props interface isn't using `uint` so neither should we here in the implementation. Please test it fixes your Windows case and lemme know.

--- src/H5Zzfp_props.c
+++ src/H5Zzfp_props.c
@@ -6,10 +6,6 @@
 #include <stdarg.h>
 #include <stdlib.h>
 
-#ifdef _MSC_VER
-typedef unsigned int uint;
-#endif
-
 #define H5Z_ZFP_PUSH_AND_GOTO(MAJ, MIN, RET, MSG)     \
 do                                                    \
 {                                                     \
@@ -58,14 +54,14 @@ static herr_t H5Pset_zfp(hid_t plist, int mode, ...)
         }
         case H5Z_ZFP_MODE_PRECISION:
         {
-            ctrls_p->details.prec = va_arg(ap, uint);
+            ctrls_p->details.prec = va_arg(ap, unsigned int);
             break;
         }
         case H5Z_ZFP_MODE_EXPERT:
         {
-            ctrls_p->details.expert.minbits = va_arg(ap, uint);
-            ctrls_p->details.expert.maxbits = va_arg(ap, uint);
-            ctrls_p->details.expert.maxprec = va_arg(ap, uint);
+            ctrls_p->details.expert.minbits = va_arg(ap, unsigned int);
+            ctrls_p->details.expert.maxbits = va_arg(ap, unsigned int);
+            ctrls_p->details.expert.maxprec = va_arg(ap, unsigned int);
             ctrls_p->details.expert.minexp  = va_arg(ap, int);
             break;
         }
