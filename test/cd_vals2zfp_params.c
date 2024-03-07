2e33c8e2502bd8ca03fe1102373eee6c9943da0e
Mark C. Miller <miller86@llnl.gov>
2024-03-06 19:25:03 -0800

    improve output info

--- test/cd_vals2zfp_params.c
+++ test/cd_vals2zfp_params.c
@@ -58,6 +58,19 @@ static void snprint_version(
         snprintf(vstr, 32, "%d.%d.%d (%d)", dig1, dig2, dig3, dig4);
 }
 
+static char const *zfp_type_str(zfp_type zt)
+{
+    switch (zt)
+    {
+        case zfp_type_none: return "none";
+        case zfp_type_int32: return "int32";
+        case zfp_type_int64: return "int64";
+        case zfp_type_float: return "float";
+        case zfp_type_double: return "double";
+    }
+    return "unknown";
+}
+
 int main(int argc, char **argv)
 {
     int i;
@@ -81,13 +94,6 @@ int main(int argc, char **argv)
             cd_vals[i-2] = (unsigned int) strtol(argv[i],0,10);
         i -= 2;
     }
-
-{
-    int q;
-    printf("cd_val0 = %u\n", cd_val0);
-    for (q = 0; q < i; q++)
-        printf("cd_vals[%d] = %u\n", q, cd_vals[q]);
-}
 
     unsigned int zfpdig1 = (cd_val0>>(16+12))&0xF;
     unsigned int zfpdig2 = (cd_val0>>(16+ 8))&0xF;
@@ -129,39 +135,79 @@ int main(int argc, char **argv)
             zfpcodec = 5; /* this is zfp 1.0.0 */
     }
 
-    printf("H5Z-ZFP plugin version %s\n", h5zfpvstr);
+    printf("H5Z-ZFP plugin version %s, ", h5zfpvstr);
     printf("ZFP library version %s (codec = %d%s)\n", zfpvstr, zfpcodec, guess?" guess":"");
 
 {
     zfp_mode zm;
 
     // argc-2: one for program name and one for for first cd_val value
-    bitstream *dummy_bstr = stream_open(&cd_vals[0], argc-2);
+    bitstream *dummy_bstr = stream_open(&cd_vals[0], sizeof(cd_vals)); 
     zfp_stream *dummy_zstr = zfp_stream_open(dummy_bstr);
     zfp_field *zfld = zfp_field_alloc();
     zfp_read_header(dummy_zstr, zfld, ZFP_HEADER_FULL);
 
     size_t dims[4];
     zfp_field_size(zfld, dims);
+    zfp_type zt = zfp_field_type(zfld);
     int ndims = (int) zfp_field_dimensionality(zfld);
+    printf("Chunked as zfp %1dD arrays of %s", ndims, zfp_type_str(zt));
+    for (int i = 0; i < ndims; i++)
+        printf("%c dims[%d]=%d", (i?',':':'),i, (int) dims[i]);
+    printf("\n");
+    zfp_field_free(zfld);
 
     // now, query stream for info you seek...
     zm = zfp_stream_compression_mode(dummy_zstr);
-    double rate = zfp_stream_rate(dummy_zstr, 1);
-    double accuracy = zfp_stream_accuracy(dummy_zstr);
-    uint precision = zfp_stream_precision(dummy_zstr);
-    unsigned int minbits, maxbits, maxprec;
-    int minexp;
-    zfp_stream_params(dummy_zstr, &minbits, &maxbits, &maxprec, &minexp);
+
+    switch (zm) 
+    {
+        case zfp_mode_null:
+        {
+            printf("zfp mode is null\n");
+            break;
+        }
+        case zfp_mode_expert:
+        {
+            unsigned int minbits, maxbits, maxprec;
+            int minexp;
+            zfp_stream_params(dummy_zstr, &minbits, &maxbits, &maxprec, &minexp);
+            printf("zfp mode is expert: minbits=%u, maxbits=%u, maxprec=%u, minexp=%d\n",
+                minbits, maxbits, maxprec, minexp);
+            break;
+        }
+        case zfp_mode_fixed_rate:
+        {
+            double rate = zfp_stream_rate(dummy_zstr, ndims);
+            printf("zfp mode is rate: rate=%g\n", rate);
+            break;
+        }
+        case zfp_mode_fixed_precision:
+        {
+            uint precision = zfp_stream_precision(dummy_zstr);
+            printf("zfp mode is precision: precision=%u\n", precision);
+            break;
+        }
+        case zfp_mode_fixed_accuracy:
+        {
+            double accuracy = zfp_stream_accuracy(dummy_zstr);
+            printf("zfp mode is accuracy: accuracy=%g\n", accuracy);
+            break;
+        }
+        case zfp_mode_reversible:
+        {
+            printf("zfp mode is reversible\n");
+            break;
+        }
+        default:
+        {
+            printf("zfp mode is unknown\n");
+            break;
+        }
+    }
     zfp_stream_close(dummy_zstr);
     stream_close(dummy_bstr);
-    printf("argc=%d, mode=%d, rate=%g, acc=%g, prec=%u\n", argc, (int)zm, rate, accuracy, precision);
-    printf("minbits=%u, maxbits=%u, maxprec=%u, minexp=%d\n", minbits, maxbits, maxprec, minexp);
-    printf("ndims = %d", ndims);
-    for (int i = 0; i < ndims; i++)
-        printf(", dims[%d] = %d", i, (int) dims[i]);
-    printf("\n");
-    zfp_field_free(zfld);
+
 }
 
     return 0;
